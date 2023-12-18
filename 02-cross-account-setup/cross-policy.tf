166c35d6587ac5ab30334ac61644a07224d549a3
Franco Navarro <francotel@Francos-MBP.lan>
2023-12-17 22:34:47 -0600

    workflow multi branch v3

--- 02-cross-account-setup/cross-policy.tf
+++ 02-cross-account-setup/cross-policy.tf
@@ -2,13 +2,14 @@ data "aws_iam_policy_document" "cross_policy" {
   statement {
     sid = "S3AccessBackend"
     actions = [
-      "s3:ListBucket"
+      "s3:ListBucket",
+      "s3:PutObject"
     ]
     resources = [
       "arn:aws:s3:::across-account-terraform-state-backend"
     ]
   }
-  
+
   statement {
     sid = "CloudWatchLogsPolicy"
     actions = [
